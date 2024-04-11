3cda5b67c53c01ee3ed8f60999e4ea010d2e3b47
Codrina Lisaru <94629883+codrinalisaru@users.noreply.github.com>
2024-04-11 08:27:25 +0300

    Add files via upload

---
+++ SI_L07t_main.c
@@ -0,0 +1,79 @@
+/* include files */
+#include "pic.h"
+
+/* contant and macro defines */
+#define LED       RC2
+#define ON        1
+#define OFF       0
+
+#define TMR1_HIGH 0xCF
+#define TMR1_LOW  0x2B
+
+/* variables */
+volatile unsigned int counter;
+
+/* function declarations */
+void init();
+
+/* function definitions */
+
+void main()
+{
+   init();
+   LED = ON; /* turn LED ON */
+   while(1)
+   {
+      /* switch LED OFF after 300 ms */
+      if(counter == 3 && LED == ON)
+      {
+         LED = OFF;
+      }
+      /* switch LED ON after 200 ms */
+      if(counter == 5 && LED == OFF)
+      {
+         LED = ON;
+         counter = 0; /* reset interrupt counter */ 
+      }   
+   }      
+}  
+
+void init()
+{
+   ANSEL = 0x0F; /* set RC0 to RC3 as digital pins */
+   ANSELH = 0x0C; /* set RC6 and RC7 as digital pins */
+   
+   TRISC = 0xF0; /* RC4 to RC7 input. RC0 to RC4 output */
+   PORTC = 0x00; /* port C pins reset value */
+   
+   /* timer 1 settings */
+   TMR1L = TMR1_LOW;
+   TMR1H = TMR1_HIGH;
+   T1CON = 0b00110000;
+   
+   /* interrupt settings */
+   GIE = 1; /* global interrupt enable */
+   PEIE = 1; /* peripheral interrupt enable */
+   TMR1IF = 0; /* clear TMR1 interrupt flag */
+   TMR1IE = 1; /* TMR1 interrupt enabled */
+   
+   /* start TMR1 */
+   TMR1ON = 1;
+   
+   /* variable intializations */
+   counter = 0;
+}
+
+/* Interrupt function */
+void interrupt isr(void)
+{
+   /* check if TMR1 interrupt enable and flag are set */
+	if((TMR1IE == 1) && (TMR1IF == 1)) 
+   {
+		TMR1L = TMR1_LOW;
+		TMR1H = TMR1_HIGH;
+		counter ++;		/* increment counter every 500ms */
+		TMR1IF = 0;		/* clear TMR1 interrupt flag*/
+	}
+}
+  
+  
